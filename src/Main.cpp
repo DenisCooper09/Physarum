09051a039f95f40bf5a7a7e4b140f12305635260
Denis Cooper <denis.cooper09@gmail.com>
2025-02-17 14:49:26 +0000

    Create GLFW window

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,58 @@
 #include <iostream>
+#include <format>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+static void GLFW_ErrorCallback(int error_code, const char *description)
+{
+    std::cerr << std::format("GLFW error: {:#x} - {}", error_code, description) << std::endl;
+}
+
+static void GLFW_FramebufferSizeCallback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
 
 int main()
 {
-    std::cout << "Hello, World\n";
+    glfwSetErrorCallback(GLFW_ErrorCallback);
+
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW." << std::endl;
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(1600, 800, "Physarum Simulation", nullptr, nullptr);
+    if (!window)
+    {
+        std::cerr << "Failed to create GLFW window." << std::endl;
+        glfwTerminate();
+        return 2;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, GLFW_FramebufferSizeCallback);
+
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
+    {
+        std::cerr << "GLAD failed to load OpenGL loader." << std::endl;
+        glfwTerminate();
+        return 3;
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.15f, 0.15f, 0.15f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
     return 0;
 }
